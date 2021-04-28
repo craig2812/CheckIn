a9a2eeb438579c7a5053769f58e93623bfdbeaa6
craig2812 <craig2812@gmail.com>
2021-04-28 13:02:29 +0100

    sidebar and notice board dummy set up

---
+++ src/DummyData/notice.js
@@ -0,0 +1,41 @@
+
+let noticeData = [
+    {
+        title:"Staff Meeting",
+        content: "We have a staff meeting on 4th May at 7pm, we look forward to seeing you there",
+        date: "Wed 28th April 2022",
+        author: "Craig Farrell"
+    },
+    {
+        title:"Lunches Today",
+        content: "Ham and Egg Sandwhiches",
+        date: "Wed 28th April 2021",
+        author: "Craig Farrell"
+    },
+    {
+        title:"Weekly Quote",
+        content: "An inspirational quote make the world go around! - Brian Dobson",
+        date: "Wed 28th April 2021",
+        author: "Craig Farrell"
+    },
+    {
+        title:"Staff Meeting",
+        content: "We have a staff meeting on 4th May at 7pm, we look forward to seeing you there",
+        date: "Wed 28th April 2024",
+        author: "Craig Farrell"
+    },
+    {
+        title:"Lunches Today",
+        content: "Ham and Egg Sandwhiches",
+        date: "Wed 28th April 2021",
+        author: "Craig Farrell"
+    },
+    {
+        title:"Weekly Quote",
+        content: "An inspirational quote make the world go around! - Brian Dobson",
+        date: "Wed 28th April 2021",
+        author: "Craig Farrell"
+    },
+]
+
+export default noticeData;
--- src/SideBar/index.js
+++ src/SideBar/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import noticeData from "../DummyData/notice"
+import "./SideBar.css"
 
 function SideBar() {
+
+let noticeDataNew = [...noticeData.slice(0,3)] // ensure use of 3 most recent posts only 
+//
     return (
         <div>
-            THIS IS SIDEBAR
-        </div>
+            {noticeDataNew.map(data =>
+          
+<div className="noticeBox">
+    <h1 className="title">{data.title}</h1>
+    <p className="content">{data.content}</p>
+    <p className="date">Posted On: {data.date}</p>
+    <p className="author">Published By: {data.author}</p>
+    </div> 
+            )}
+
+       </div>
     )
 }
 
